6b0772c9bda7d2a74b9052ccdb4a36773a814ebf
Enigma <jonpking@gmail.com>
2019-06-18 15:47:52 -0700

    wrapped code in 'document.ready' function, added code for timer functionality and for first question and answer set to appear after start button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,119 @@
+$(document).ready(function () {
 
-let correct = 0;
-let incorrect = 0;
-let unanswered = 0;
-let timer = 20;
-let timerRunning = false;
-const trivia = {
-// questions asked
-    questions: {
-        q1: "In Ocarina of Time, which is NOT on of the three spells of the Goddesses that can be acquired?",
-        q2: "Who is the main villain in Skyward Sword'?",
-        q3: "After whom is the character Zelda named?",
-        q4: "Which of the following is NOT a part of the Triforce?",
-        q5: "In A Link to the Past, which item, other than the Master Sword can be used to reflect Agahnim's spells?",
-        q6: "What is the name of Zelda's alter ego in Ocarina of time?",
-        q7: "What is the name of Link's evil doppelganger?",
-        q8: "In Twilight Princess, into what animal does link transform?",
-        q9: "What is the value of a red rupee?",
-        q10: "In Wind Waker, what is the real identity of the King of Red Lions?",
-    },
-// correct answer to each question
-    answers: {
-        q1: "Hylia's Strength",
-        q2: "Ghirahim",
-        q3: "Zelda Fitzgerald",
-        q4: "Honor",
-        q5: "Bug-Catching Net",
-        q6: "Sheik",
-        q7: "Dark Link",
-        q8: "Wolf",
-        q9: "20",
-        q10: "King Daphnes",
-    },
-// answers that can be selected for each question
-    selections: {
-        q1: ["Din's Fire", "Nayru's Love", "Farore's Wind", "Hylia's Strength"],
-        q2: ["Ganon", "Ghirahim", "Vaati", "Zant"],
-        q3: ["Zelda Fitzgerald", "Zelda Williams", "Zelda Rubinstein", "Zelda Spellman"],
-        q4: ["Honor", "Wisdom", "Courage", "Power"],
-        q5: ["Hookshot", "Boomerang", "Hammer", "Bug-Catching Net"],
-        q6: ["Ruto", "Impa", "Sheik", "Tetra"],
-        q7: ["Evil Link", "Knil", "He does not have a name", "Dark Link"],
-        q8: ["Monkey", "Wolf", "Cat", "Hawk"],
-        q9: ["100", "5", "20", "1"],
-        q10: ["King Daphnes", "Princess Zelda", "Goddess Hylia", "Sage Sahasrahla"],
-    }
+    let correct = 0;
+    let incorrect = 0;
+    let unanswered = 0;
+    let timer = 20;
+    let timerRunning = false;
+    let timerInterval;
+    const trivia = {
+        // questions asked
+        questions: {
+            q1: "In Ocarina of Time, which is NOT on of the three spells of the Goddesses that can be acquired?",
+            q2: "Who is the main villain in Skyward Sword'?",
+            q3: "After whom is the character Zelda named?",
+            q4: "Which of the following is NOT a part of the Triforce?",
+            q5: "In A Link to the Past, which item, other than the Master Sword can be used to reflect Agahnim's spells?",
+            q6: "What is the name of Zelda's alter ego in Ocarina of time?",
+            q7: "What is the name of Link's evil doppelganger?",
+            q8: "In Twilight Princess, into what animal does link transform?",
+            q9: "What is the value of a red rupee?",
+            q10: "In Wind Waker, what is the real identity of the King of Red Lions?",
+        },
+        // correct answer to each question
+        answers: {
+            q1: "Hylia's Strength",
+            q2: "Ghirahim",
+            q3: "Zelda Fitzgerald",
+            q4: "Honor",
+            q5: "Bug-Catching Net",
+            q6: "Sheik",
+            q7: "Dark Link",
+            q8: "Wolf",
+            q9: "20",
+            q10: "King Daphnes",
+        },
+        // answers that can be selected for each question
+        selections: {
+            q1: ["Din's Fire", "Nayru's Love", "Farore's Wind", "Hylia's Strength"],
+            q2: ["Ganon", "Ghirahim", "Vaati", "Zant"],
+            q3: ["Zelda Fitzgerald", "Zelda Williams", "Zelda Rubinstein", "Zelda Spellman"],
+            q4: ["Honor", "Wisdom", "Courage", "Power"],
+            q5: ["Hookshot", "Boomerang", "Hammer", "Bug-Catching Net"],
+            q6: ["Ruto", "Impa", "Sheik", "Tetra"],
+            q7: ["Evil Link", "Knil", "He does not have a name", "Dark Link"],
+            q8: ["Monkey", "Wolf", "Cat", "Hawk"],
+            q9: ["100", "5", "20", "1"],
+            q10: ["King Daphnes", "Princess Zelda", "Goddess Hylia", "Sage Sahasrahla"],
+        }
+    };
 
-};
+    // hide start button and display first question
+    function startGame() {
+        resetGame();
+        startTimer();
+        $("#startButton").click(function(){
+            $("#gameArea").html("<h4 class='text-center mt-3 font-weight-bolder'>" + trivia.questions.q1 + "</h4>")
+            for(let i = 0; i < 4; i++){
+                $("#gameArea").append("<button class='col-8 mx-auto m-2'>" + trivia.selections.q1[i] + "</button>")
+            }
+        });
+    };
 
-// hide start button and display first question
-function startGame(){
+    // reset the game to default state
+    function resetGame() {
+        correct = 0;
+        incorrect = 0;
+        unanswered = 0;
+    };
 
-};
+    //start timer
+    function startTimer() {
+        $("#timeRemainingAmount").text(timer);
+        clearInterval(timerInterval);
+        timerInterval = setInterval(decrementTimer, 1000);
+    };
 
-// reset the game to default state
-function resetGame(){
+    // reset the timer
+    function resetTimer() {
 
-};
+    };
 
-//start timer
-function startTimer(){
+    // decrement timer
+    function decrementTimer() {
+        timer--;
+        $("#timeRemainingAmount").text(timer);
+        if(timer === 0){
+            stopTimer();
+        }
+    };
 
-};
+    // stop timer
+    function stopTimer() {
+        clearInterval(timerInterval);
+    };
 
-// reset the timer
-function resetTimer(){
+    // cycle to next question
+    function nextQuestion() {
 
-};
+    };
 
-// cycle to next question
-function nextQuestion(){
+    // check if answer if correct
+    function answerCheck() {
 
-};
+    };
 
-// check if answer if correct
-function answerCheck(){
+    // increment correct answer variable
+    function answerCorrect() {
 
-};
+    };
 
-// increment correct answer variable
-function answerCorrect(){
+    // increment incorrect answer variable
+    function answerIncorrect() {
 
-};
+    };
 
-// increment incorrect answer variable
-function answerIncorrect(){
 
-};
+    startGame();
+
+});
