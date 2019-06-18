2e25738d26d8502190bc02bc71f7008d1de73675
Enigma <jonpking@gmail.com>
2019-06-18 12:31:27 -0700

    set up variables and objects for questions and tracking

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+
+let correct = 0;
+let incorrect = 0;
+let unanswered = 0;
+
+const trivia = {
+    questions: {
+        q1: "In Ocarina of Time, which is NOT on of the three spells of the Goddesses that can be acquired?",
+        q2: "Who is the main villain in Skyward Sword'?",
+        q3: "After whom is the character Zelda named?",
+        q4: "Which of the following is NOT a part of the Triforce?",
+        q5: "In A Link to the Past, which item, other than the Master Sword can be used to reflect Agahnim's spells?",
+        q6: "What is the name of Zelda's alter ego in Ocarina of time?",
+        q7: "What is the name of Link's evil doppelganger?",
+        q8: "In Twilight Princess, into what animal does link transform?",
+        q9: "What is the value of a red rupee?",
+        q10: "In Wind Waker, what is the real identity of the King of Red Lions?",
+    },
+    answers: {
+        q1: "Hylia's Strength",
+        q2: "Ghirahim",
+        q3: "Zelda Fitzgerald",
+        q4: "Honor",
+        q5: "Bug-Catching Net",
+        q6: "Sheik",
+        q7: "Dark Link",
+        q8: "Wolf",
+        q9: "20",
+        q10: "King Daphnes",
+    },
+    selections: {
+        q1: ["Din's Fire","Nayru's Love","Farore's Wind","Hylia's Strength"],
+        q2: ["Ganon","Ghirahim","Vaati","Zant"],
+        q3: ["Zelda Fitzgerald","Zelda Williams","Zelda Rubinstein","Zelda Spellman"],
+        q4: ["Honor","Wisdom","Courage","Power"],
+        q5: ["Hookshot","Boomerang","Hammer","Bug-Catching Net"],
+        q6: ["Ruto","Impa","Sheik","Tetra"],
+        q7: ["Evil Link","Knil","He does not have a name","Dark Link"],
+        q8: ["Monkey","Wolf","Cat","Hawk"],
+        q9: ["100","5","20","1"],
+        q10: ["King Daphnes","Princess Zelda","Goddess Hylia","Sage Sahasrahla"],
+    }
+
+};
